b04bcea37aa095f5334a9a665228353c0adde61e
tunnckoCore <mameto_100@mail.bg>
2015-06-29 04:09:17 +0300

    implement :blush:

---
+++ index.js
@@ -0,0 +1,18 @@
+/*!
+ * is-singular <https://github.com/tunnckoCore/is-singular>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+var pluralize = require('pluralize')
+
+module.exports = function isSingular (word) {
+  if (typeof word !== 'string') {
+    return false
+  }
+
+  return word === pluralize(word, 1)
+}
---
+++ test.js
@@ -0,0 +1,34 @@
+/*!
+ * is-singular <https://github.com/tunnckoCore/is-singular>
+ *
+ * Copyright (c) 2015 Charlike Mike Reagent <@tunnckoCore> (http://www.tunnckocore.tk)
+ * Released under the MIT license.
+ */
+
+/* jshint asi:true */
+
+'use strict'
+
+var test = require('assertit')
+var isSingular = require('./index')
+
+test('is-singular:', function () {
+  test('should return true if word is in singular form, like `apple`', function (done) {
+    test.equal(isSingular('apple'), true)
+    test.equal(isSingular('test'), true)
+    test.equal(isSingular('car'), true)
+    done()
+  })
+  test('should return false if word not in single form like `apples`', function (done) {
+    test.equal(isSingular('apples'), false)
+    test.equal(isSingular('tests'), false)
+    test.equal(isSingular('cars'), false)
+    done()
+  })
+  test('should return false if not a string given', function (done) {
+    test.equal(isSingular(123), false)
+    test.equal(isSingular([1, 2]), false)
+    test.equal(isSingular({a: 'b'}), false)
+    done()
+  })
+})
